01ae374ab251010a183c91ee0f27355993daa514
Dio6o <lohn.diogo@gmail.com>
2026-03-03 19:50:46 -0300

    sorting database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,29 +4,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.ArrayList;
 
-//TODO scan the data base for duplicates (Separated class), choose between the values at random.
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Random random = new Random();
 
-//        Sorter sorter = new Sorter();
-//        sorter.queryData();
+        Sorter sorter = new Sorter();
+        sorter.queryData();
 
-        ArrayList<String[]> duplicates = new ArrayList<>();
-        String[] a = {"Kepler-33 f","2011","Transit","41.02800000000","4.00000000","9.60000000","1209.16000000"};
-        String[] b = {"Kepler-33 f","2011","Transit","41.02900000000","4.00000000","9.65000000","1209.16000000"};
-        String[] c = {"Kepler-33 f","2011","Transit","41.02806370000","4.14000000","","1209.16000000"};
-        String[] d = {"Kepler-33 f","2011","Transit","","3.83100000","","1209.16000000"};
-
-        duplicates.add(a);
-        duplicates.add(b);
-        duplicates.add(c);
-        duplicates.add(d);
-
-        for (int i = 0; i<duplicates.size();i++) {
-
-        }
     }
 }
--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -6,10 +6,13 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Sorter {
 
     //TODO scan the data base for duplicates (Separated class), choose between the values at random.
+
+    Random random = new Random();
 
     final String URLDATABASE = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=";
 
@@ -17,65 +20,107 @@ public class Sorter {
             "+from+ps&format=csv";
     final String OUTPUT = "saida.txt";
 
-    public void queryData(){
+    public void queryData() {
         try {
 
-            if (!fileExists(OUTPUT)){
+            if (!fileExists(OUTPUT)) {
 
-            URL url = new URL(URLDATABASE + SQLSTATEMENT);
-            URLConnection connection = url.openConnection();
-            FileWriter file = new FileWriter(OUTPUT);
+                URL url = new URL(URLDATABASE + SQLSTATEMENT);
+                URLConnection connection = url.openConnection();
+                FileWriter file = new FileWriter(OUTPUT);
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
 
-            String[] currentLine;
-            String[] lastLine = {""};
-            ArrayList<String[]> duplicates = new ArrayList<>();
+                String[] currentLine;
+                String[] lastLine = {""};
+                ArrayList<String[]> duplicates = new ArrayList<>();
 
-
-
-            while ((inputLine = reader.readLine()) != null) {
-
-                currentLine = inputLine.split(",");
+                //remove later
+                int x = 0;
 
 
-                if (currentLine[0].equals(lastLine[0])) {
-                    duplicates.add(lastLine);
+                while ((inputLine = reader.readLine()) != null) {
 
-                } else if(duplicates.size() > 1) {
+                    currentLine = inputLine.split(",");
 
-                    for (int i = 0; i < duplicates.size(); i++) {
-                        System.out.println(Arrays.toString(duplicates.get(i)));
+                    if (currentLine[0].equals(lastLine[0])) {
+                        duplicates.add(lastLine);
+
+                    } else if (duplicates.size() > 1) {
+
+                        String item = "";
+                        String[] itemData = {};
+                        int randomNumber;
+                        int blankPosition;
+
+                        blankPosition = containsBlank(duplicates.getFirst());
+
+
+                        if (blankPosition >= 0) {
+
+                            int i = 0;
+
+                            do {
+                                randomNumber = random.nextInt(duplicates.size());
+                                itemData = duplicates.get(randomNumber);
+                                if (itemData.length < 7){
+                                    break;
+                                }
+                                item = itemData[blankPosition];
+                                itemData[blankPosition] = item;
+
+                                if(i >= 10){
+                                    item = "0";
+                                }
+
+                                duplicates.set(0, itemData);
+
+                                i++;
+                            } while (item.isBlank());
+
+                        }
+
+                        file.write(Arrays.toString(duplicates.getFirst()) + "\n");
+                        duplicates.clear();
+
+                    } else {
+
+                        file.write(inputLine + "\n");
+
                     }
-                    System.out.println();
 
-
-                    // dont delete !!!
-                    duplicates.clear();
-
+                    lastLine = currentLine;
                 }
 
-
-                file.write(inputLine + "\n");
-
-                lastLine = currentLine;
-            }
-
-            file.close();
-            reader.close();
+                file.close();
+                reader.close();
 
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public boolean fileExists(String filename){
+    public boolean fileExists(String filename) {
         File file = new File(filename);
+        //TODO remove comment
         //return file.exists();
         return false;
     }
 
+    public int containsBlank(String[] data) {
+        /*
+          Searches the array for a blank value, if the value is found return de position of the blank, else return -1.
+         */
+        for (int i = 0; i < data.length; i++) {
+            if (data[i].isBlank()) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
+
